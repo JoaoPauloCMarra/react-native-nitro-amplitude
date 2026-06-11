3bd53a103d18784749c92fe936835590e148b507
Joao Paulo Costa Marra <joaopaulocmarra@gmail.com>
2026-06-11 17:21:29 -0300

    fix: initialize Android amplitude from package (#6)

--- packages/react-native-nitro-amplitude/app.plugin.js
+++ packages/react-native-nitro-amplitude/app.plugin.js
@@ -1,36 +1,11 @@
-const { withMainApplication, createRunOncePlugin } = require("@expo/config-plugins");
+const { createRunOncePlugin } = require("@expo/config-plugins");
+const pkg = require("./package.json");
 
-const withNitroAmplitude = (config) => {
-  return withMainApplication(config, (config) => {
-    const { modResults } = config;
-    const { language, contents } = modResults;
-
-    if (language === "kt") {
-      if (!contents.includes("AndroidAmplitudeAdapter.setContext")) {
-        const importStatement =
-          "import com.nitroamplitude.AndroidAmplitudeAdapter";
-        const initStatement = "    AndroidAmplitudeAdapter.setContext(this)";
-
-        if (!contents.includes(importStatement)) {
-          modResults.contents = contents.replace(
-            /(package .*\n)/,
-            `$1\n${importStatement}\n`,
-          );
-        }
-
-        modResults.contents = modResults.contents.replace(
-          /(super\.onCreate\(\))/,
-          `$1\n${initStatement}`,
-        );
-      }
-    }
-
-    return config;
-  });
-};
+const withNitroAmplitude = (config) => config;
 
 module.exports = createRunOncePlugin(
   withNitroAmplitude,
-  "react-native-nitro-amplitude",
-  "1.0.0",
+  pkg.name,
+  pkg.version,
 );
+module.exports.withNitroAmplitude = withNitroAmplitude;
